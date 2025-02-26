b6b29fce09683949965bb867996571ccfb9aceaf
Ann Mary Sruthy <annmarysruthy2401@gmail.com>
2025-02-26 15:13:46 +0530

    Activity 1

---
+++ web/modules/cache_handson/src/Plugin/Block/LatestArticlesBlock.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\cache_handson\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides 'Latest Articles' block displaying the title of last 3 articles created.
+ *
+ * @Block(
+ *   id = "latest_articles_block",
+ *   admin_label = @Translation("Latest Articles"),
+ * )
+ */
+class LatestArticlesBlock extends BlockBase implements ContainerFactoryPluginInterface {
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected EntityTypeManagerInterface $entityTypeManager;
+  
+  /**
+   * Constructs a new LatestArticlesBlock instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the block.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array{
+    $query = $this->entityTypeManager->getStorage('node')->getQuery()
+    -> condition('status', 1)
+    -> condition('type', 'article')
+    -> sort('created', 'DESC')
+    -> range(0, 3)
+    ->accessCheck(FALSE);
+
+    $nids = $query->execute();
+    $nodes = $this->entityTypeManager->getStorage('node')->loadMultiple($nids);
+
+    foreach ($nodes as $node) {
+      $items[] = ['#markup' => $node->label()];
+      $cache_tags[] = 'node:' . $node->id();
+    }
+
+    return [
+      '#theme' => 'item_list',
+      '#items' => $items,
+      '#cache' => [
+        'tags' => $cache_tags,
+      ]
+    ];
+  }
+}
